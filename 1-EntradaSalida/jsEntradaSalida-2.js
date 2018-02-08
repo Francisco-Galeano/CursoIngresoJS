3d50a1e35b5f3799441b2f057f2ee9b55b1ebe65
Francisco Galeano <francisco.galeano@hotmail.com.ar>
2018-02-08 20:39:53 -0300

    Ejercicio 1 y 2
    Aprendimos alert, prompt, var, valores literales, undefined.

--- 1-EntradaSalida/jsEntradaSalida-2.js
+++ 1-EntradaSalida/jsEntradaSalida-2.js
@@ -2,6 +2,12 @@
 y luego mostrarlo por 'alert' al presionar el botón  'MOSTRAR'*/
 function Mostar()
 {
-	
+	var nombre;
+	nombre = prompt("Ingrese su nombre", "Aquí escribís tu nombre");
+	alert("Buenos días " + nombre + ", ¿cómo estás?");
+	/*alert("nombre");
+	var person = prompt("Please enter your name", "Harry Potter");
+	alert(person);*/
+
 }
 

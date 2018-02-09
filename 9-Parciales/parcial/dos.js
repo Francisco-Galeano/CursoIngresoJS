3bfcd96550fd3c6fa91a9297e890a8e2406f0641
Francisco Galeano <francisco.galeano@hotmail.com.ar>
2018-02-09 20:37:28 -0300

    Ejercicio 2 resuelto

--- 9-Parciales/parcial/dos.js
+++ 9-Parciales/parcial/dos.js
@@ -1,4 +1,14 @@
 function Mostrar()
 {
-  
+  var importe;
+
+  importe = prompt("Ingrese aquí el precio del producto", "Ejemplo: $100");
+
+  var iva;
+
+  iva = importe*(1.21);
+
+  document.getElementById('importeFinal').value = iva;
+
+
 }

7e84ee198cad2dfa70912446611bdbd395fae086
Francisco Galeano <francisco.galeano@hotmail.com.ar>
2018-02-09 20:05:23 -0300

    Ejercicio 10

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -4,5 +4,16 @@ mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function MostrarAumento()
 {
-	
+	var importe;
+
+	importe = document.getElementById('importe').value;
+
+	importe = parseInt(importe);
+
+	var descuento;
+
+	descuento = importe/100*75;
+
+	document.getElementById("resultado").value = descuento;
+
 }

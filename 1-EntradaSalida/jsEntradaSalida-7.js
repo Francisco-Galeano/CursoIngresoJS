5544b0834ef4d6f279b149b0f861bf668d69227e
Francisco Galeano <francisco.galeano@hotmail.com.ar>
2018-02-09 19:33:57 -0300

    Ejercicio 7 y 8
    Re-subidos

--- 1-EntradaSalida/jsEntradaSalida-7.js
+++ 1-EntradaSalida/jsEntradaSalida-7.js
@@ -4,21 +4,78 @@ mostar el resulto por medio de "ALERT"
 ej.: "la Resta es 750"*/ 
 function sumar()
 {	
+	var numeroUno;
+
+	var numeroDos;
 	
+	numeroUno = document.getElementById("numeroUno").value;
+	
+	numeroDos = document.getElementById("numeroDos").value;
+
+	numeroUno = parseInt(numeroUno);
+	
+	numeroDos = parseInt(numeroDos);
+
+	var suma = numeroUno + numeroDos;
+
+	alert(suma);
+
 }
 
 function restar()
 {
+	var numeroUno;
+
+	var numeroDos;
 	
+	numeroUno = document.getElementById("numeroUno").value;
+	
+	numeroDos = document.getElementById("numeroDos").value;
+
+	numeroUno = parseInt(numeroUno);
+	
+	numeroDos = parseInt(numeroDos);
+
+	var resta = numeroUno - numeroDos;
+
+	alert(resta);
 }
 
 function multiplicar()
 { 
+	var numeroUno;
+
+	var numeroDos;
 	
+	numeroUno = document.getElementById("numeroUno").value;
+	
+	numeroDos = document.getElementById("numeroDos").value;
+
+	numeroUno = parseInt(numeroUno);
+	
+	numeroDos = parseInt(numeroDos);
+
+	var multiplicacion = numeroUno * numeroDos;
+
+	alert(multiplicacion);	
 }
 
 function dividir()
 {
+	var numeroUno;
+
+	var numeroDos;
 	
+	numeroUno = document.getElementById("numeroUno").value;
+	
+	numeroDos = document.getElementById("numeroDos").value;
+
+	numeroUno = parseInt(numeroUno);
+	
+	numeroDos = parseInt(numeroDos);
+
+	var division = numeroUno / numeroDos;
+
+	alert(division);
 }
 

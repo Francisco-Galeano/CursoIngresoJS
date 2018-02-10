30f9cdf7bd22e7825a0d22826d2206276f77e5f9
Francisco Galeano <francisco.galeano@hotmail.com.ar>
2018-02-09 21:50:42 -0300

    Ejercicio 3
    Introducción del else

--- 2-InstruccionIf/jsInstruccionIF (3).js
+++ 2-InstruccionIf/jsInstruccionIF (3).js
@@ -1,5 +1,23 @@
 function Mostrar()
 {
+	var edad;
+
+	edad = document.getElementById('edad').value;
+
+	if (edad >= "18"){
+
+		alert("Usted es mayor de edad.")
+
+		}
+
+		else{
+
+			alert("Usted es menor de edad.")
+
+	}
+	
+
+
 //tomo la edad  
 
 

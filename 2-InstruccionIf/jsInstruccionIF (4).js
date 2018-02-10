69ff6061eaa54c9f1c71ed3cf95a86caae88e5ee
Francisco Galeano <francisco.galeano@hotmail.com.ar>
2018-02-09 22:05:08 -0300

    Ejercicio 4 y 5
    Ejercicios con varias condiciones en un if

--- 2-InstruccionIf/jsInstruccionIF (4).js
+++ 2-InstruccionIf/jsInstruccionIF (4).js
@@ -1,5 +1,13 @@
 function Mostrar()
 {
+	var edad;
+
+	edad = document.getElementById('edad').value;
+
+	if(edad >= "13" && edad <= "17"){
+
+		alert("Sos un adolescente, anda a la plaza.")
+	}
 //tomo la edad  
 
 

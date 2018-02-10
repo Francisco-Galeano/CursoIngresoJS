025d820ef5bcd9d67002f479803d8a3168aa71fa
Francisco Galeano <francisco.galeano@hotmail.com.ar>
2018-02-09 21:06:30 -0300

    Ejercicio 3 resuelto

--- 9-Parciales/parcial/tres.js
+++ 9-Parciales/parcial/tres.js
@@ -1,4 +1,29 @@
 function Mostrar()
 {
+	var largo;
+
+	var ancho;
+
+	largo = document.getElementById('alrgo').value;
+
+	ancho = document.getElementById("ancho").value;
+
+	largo = parseInt(largo);
+
+	ancho = parseInt(ancho);
+
+	var perimetro;
+
+	perimetro = largo*2 + ancho*2;
+
+	var alambre;
+
+	alambre = perimetro*(3);
+
+	alert("Tu terreno tiene un perímetro de " + perimetro + " metros. La cantidad de alambre necesaria es " + alambre + " metros.");
+
+
+
+
 
 }

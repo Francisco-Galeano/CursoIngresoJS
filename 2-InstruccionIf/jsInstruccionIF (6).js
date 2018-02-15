b391c810171131ec1b4d15ca6de631c6f9a92b1b
Francisco Galeano <francisco.galeano@hotmail.com.ar>
2018-02-15 19:35:27 -0300

    Ejercicio 6
    Como ahorrar hacer 3 "if"

--- 2-InstruccionIf/jsInstruccionIF (6).js
+++ 2-InstruccionIf/jsInstruccionIF (6).js
@@ -1,8 +1,30 @@
 function Mostrar()
 {
-//tomo la edad  
+	var edad;
+
+	edad = document.getElementById('edad').value;
+
+	if(edad < 13){
+
+		alert("Eres un niño.");
+
+	}
+
+	else{
+
+		if(edad > 17){
+
+			alert("Ud es mayor de edad.");
+
+		}
+
+		else{
+
+			alert("Sos un adolescente.");
+
+		}
+
+	}
 
 
-
-
-}//FIN DE LA FUNCIÓN
+}

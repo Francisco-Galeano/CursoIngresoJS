089fa43506d195b3692fb0c306ce202274d66c61
Francisco Galeano <francisco.galeano@hotmail.com.ar>
2018-02-15 19:55:38 -0300

    Ejercicio 7

--- 2-InstruccionIf/jsInstruccionIF (7).js
+++ 2-InstruccionIf/jsInstruccionIF (7).js
@@ -1,5 +1,18 @@
 function Mostrar()
 {
+
+	var edad;
+
+	var estadoCivil;
+
+	edad = document.getElementById('edad').value;
+
+	estadoCivil = document.getElementById("estadoCivil").value;
+
+	if(edad < 18 && estadoCivil != "Soltero"){
+
+		alert("Es muy pequeño para NO estar soltero.");
+	}
 //tomo la edad  
 
 	

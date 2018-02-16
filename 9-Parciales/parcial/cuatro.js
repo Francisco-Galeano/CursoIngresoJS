d3c0ab05d5da6a57eef3b51297e75c759fbb7d13
Francisco Galeano <francisco.galeano@hotmail.com.ar>
2018-02-16 19:32:11 -0300

    Ejercicio 4 resuelto

--- 9-Parciales/parcial/cuatro.js
+++ 9-Parciales/parcial/cuatro.js
@@ -1,4 +1,41 @@
 function Mostrar()
 {
 
+	var primerNumero;
+	var	segundoNumero;
+	var multiplicacion;
+	var suma;
+	var resta;
+
+	primerNumero = prompt("Primer número", "Ejemplo: 1000");
+	segundoNumero = prompt("Segundo número", "Ejemplo: 500");
+	multiplicacion = parseInt(primerNumero) * parseInt(segundoNumero);
+	suma = parseInt(primerNumero) + parseInt(segundoNumero);
+	resta = parseInt(primerNumero) - parseInt(segundoNumero);
+
+
+	if(primerNumero == segundoNumero){
+
+				document.write(multiplicacion);
+
+	}
+	else{
+
+		if(primerNumero > segundoNumero){
+
+			document.write(resta);
+
+		}
+		else{
+
+			document.write(suma);
+				
+			}
+
+			
+
+		}
+
+	
+
 }

eec41128f405cfd68f702ab1f38a9ae8d0548d16
Francisco Galeano <francisco.galeano@hotmail.com.ar>
2018-02-16 21:33:10 -0300

    Ejercicio del TP resuelto

--- 8-TPs/jsAdivinaElNumero1.js
+++ 8-TPs/jsAdivinaElNumero1.js
@@ -9,9 +9,21 @@ de no ser igual se debe informar si “falta…”  para llegar al número secre
 */
 var numeroSecreto; 
 var contadorIntentos;
+var numero;
 
+	
 function comenzar()
 {
+	
+	numeroSecreto = Math.floor((Math.random()*100)+1);
+	
+	contadorIntentos = 0;
+
+	console.log(numeroSecreto);
+
+
+
+
 	//Genero el número RANDOM entre 1 y 100
 	 
 		//alert(numeroSecreto );
@@ -21,6 +33,76 @@ function comenzar()
 
 function verificar()
 {
+
+	numero = document.getElementById('numero').value;
+
+	contadorIntentos = contadorIntentos + 1;
+
+	document.getElementById('intentos').value = contadorIntentos;
+
 	
-	
+
+	if(contadorIntentos > 9){
+
+		return alert("-GAME OVER-");
+		
+	}
+
+	if(numeroSecreto == numero && contadorIntentos == 1){
+
+		alert("NOPE");
+	}
+	else{
+
+		if(numeroSecreto == numero){
+
+			alert("Usted es el ganador! Y en sólo " + contadorIntentos + " intentos.");
+
+		}
+		else{
+
+			if(numeroSecreto < numero){
+
+				alert("Te pasaste!");
+
+			}
+			else{
+
+				alert("Te faltan!");
+
+			}
+
+		}
+
+	}
+
 }
+
+			
+		
+	
+
+
+	/*if(numeroSecreto == numero){
+
+		alert("Usted es el ganador! Y en sólo " + contadorIntentos + " intentos.");
+
+	}
+	else{
+
+		if(numeroSecreto < numero){
+
+			alert("Número equivocado! Te pasaste por algunos...");
+
+		}
+		else{
+
+			alert("Número equivocado! Te faltan algunos...");
+		}
+
+		
+
+
+	}
+	
+}*/

d217bba4aa0d3ebd0a68c42610cb1eccd9edd18f
Francisco Galeano <francisco.galeano@hotmail.com.ar>
2018-02-16 21:53:57 -0300

    Ejercicios del 1 al 6

--- 3-InstruccionSwitch/jsInstruccionSwitch(3).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(3).js
@@ -4,7 +4,19 @@ function Mostrar()
 var mesDelAño = document.getElementById('mes').value;
 
 //alert (mesDelAño);
-	
+
+switch(mesDelAño){
+
+	case "Febrero":
+		alert("Este mes no tiene más de 29 días");
+		break;
+
+	default:
+		alert("Este mes tiene más de 30 días");
+		break;
+
+
+}
 	
 
 

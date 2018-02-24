dd063740b77ce6a0e540d40cd9b4b0dd100d04e9
Francisco Galeano <francisco.galeano@hotmail.com.ar>
2018-02-23 21:45:07 -0300

    Ejercicios 9 y 10

--- 5-InstruccionWhile/jsIteraciones (10).js
+++ 5-InstruccionWhile/jsIteraciones (10).js
@@ -3,13 +3,51 @@ function Mostrar()
 
 	var contador=0;
 	//declarar contadores y variables 
+	var numero;
+	var acumuladorNegativos;
+	var acumuladorPositivos;
+	var contadorNegativos = 0;
+	var contadorPositivos = 0;
+	var contadorCeros = 0;
+	var contadorPares = 0;
+	var diferencia;
 	
 	var respuesta="si";
 
-	while(respuesta!="no")
-	{
+	while(respuesta!="no"){
+
+		if(numero < 0){
+
+			contadorNegativos = contadorNegativos + 1;
+			acumuladorNegativos = acumuladorNegativos + numero;
 		
-	
+		}
+		else{
+
+			if(numero > 0){
+
+				contadorPositivos = contadorPositivos + 1;
+				acumuladorPositivos = acumuladorPositivos + numero;
+
+			}
+			else{
+
+				if(numero == 0){
+
+					contadorCeros = contadorCeros + 1;
+
+				}
+
+				if(numero / 2 == 0){
+
+					contadorPares = contadorPares + 1;
+
+				}
+
+				
+			}
+
+		}
 	}
 
 
--- 5-InstruccionWhile/jsIteraciones (9).js
+++ 5-InstruccionWhile/jsIteraciones (9).js
@@ -8,39 +8,50 @@ function Mostrar()
 	var minimo;
 	var respuesta='si';
 
-		while(respuesta!='no'){
+	while(respuesta!='no'){
 
-			contador = contador + 1;
-			numero = prompt("Ingrese un número");
-			numero = parseInt(numero);
+		contador = contador + 1;
+		numero = prompt("Ingrese un número");
+		numero = parseInt(numero);
 
-			while(isNaN(numero)){
+		while(isNaN(numero)){
 
-			numero = prompt("Por favor, ingrese un número.");
-			numero = parseInt(numero);
+		numero = prompt("Por favor, ingrese un número.");
+		numero = parseInt(numero);
+
+		}
+
+		respuesta = prompt("Desea continuar?");
+
+		if(contador = 1){
+
+			minimo == numero;
+			maximo == numero;
+				
+			}
+			else{
+
+				if(numero > maximo){
+
+					maximo = numero;
+					
+				}
+				else{
+						
+					if(numero < minimo){
+
+						minimo = numero;
+
+					}
+
+				}
 
 			}
 
-			respuesta = prompt("Desea continuar?");
-
-			if(contador){
-
-				minimo = numero;
-				maximo = numero;
-
-			}
-
-		
-
-			
+		}
 
 
-		
-	
-	}
-
-
-document.getElementById('maximo').value = maximo;
-document.getElementById('minimo').value = minimo;
+	document.getElementById('maximo').value = maximo;
+	document.getElementById('minimo').value = minimo;
 
 }//FIN DE LA FUNCIÓN

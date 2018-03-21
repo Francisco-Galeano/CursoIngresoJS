cea5af4523908d23e261065a29795a5629f1e2db
Francisco Galeano <francisco.galeano@hotmail.com.ar>
2018-03-20 22:21:10 -0300

    C en Codeblocks
    Me los llevo por las dudas

---
+++ Clase 1 Ejercicio 2/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+
+    char apellido;
+    float sueldo;
+    int aumento;
+    float sueldoFinal;
+
+    printf("Ingrese aqui el apellido \n");
+    scanf("%c", &apellido);
+    printf("Ingrese el sueldo \n");
+    scanf("%f", &sueldo);
+    printf("Ingrese un aumento \n");
+    scanf("%d", &aumento);
+    sueldoFinal = sueldo + (sueldo/100)*aumento;
+    printf("El apellido es: %c El sueldo es: %f El sueldo final es: %f", apellido, sueldo, sueldoFinal );
+
+    return 0;
+}
